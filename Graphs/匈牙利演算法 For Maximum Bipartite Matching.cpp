d66287ebdc08322cb387901cd9dfb4816aedbaad
Phoenix0423 <34877707+EricHuang2006@users.noreply.github.com>
2023-03-22 20:04:53 +0800

    Update 匈牙利演算法 For Maximum Bipartite Matching.cpp

--- Graphs/匈牙利演算法 For Maximum Bipartite Matching.cpp
+++ Graphs/匈牙利演算法 For Maximum Bipartite Matching.cpp
@@ -1,46 +1,72 @@
-//see: https://cp-algorithms.com/graph/kuhn_maximum_bipartite_matching.html#kuhns-algorithm
-//Kuhn's algorithm is a subroutine in the Hungarian algorithm, also known as the Kuhn-Munkres algorithm.
+//TIOJ 2197. E. 密室逃脫 https://tioj.ck.tp.edu.tw/problems/2197
 #include<bits/stdc++.h>
 using namespace std;
-int n, k; // n = number of vertices in the first part, k = number of vertices in the second part
-vector<vector<int>> adj;
-vector<int> mt;
-vector<bool> used;
+const int maxn = 600;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+ll lx[600], ly[600], vx[600], vy[600], match[600];
+int n;
+vector<vector<ll>> w;
 
-bool try_kuhn(int v){
-	if(used[v]) return false;
-	used[v] = true;
-	for(auto x : adj[v]){
-		if(mt[x] == -1 || try_kuhn(mt[x])){
-			mt[x] = v;
-			return true;
-		}
-	}
-	return false;
+int dfs(int x){
+    //cout<<"dfs : "<<x<<endl;
+    if(vx[x]) return false;
+    vx[x] = 1;
+    for(int i = 0; i < n; i++){
+        if(lx[x] + ly[i] == w[x][i] && !vy[i]){
+            vy[i] = true;
+            if(match[i] == -1 || dfs(match[i])){
+                match[i] = x;
+                return true;
+            }
+        }
+
+    }
+    return false;
 }
+
+void relabel(){
+    int mn = 1e9;
+    for(int i = 0; i < n; i++){
+        if(!vx[i]) continue;
+        for(int j = 0; j < n; j++){
+            if(vy[j]) continue;
+            int val = lx[i] + ly[j] - w[i][j];
+            mn = min(mn, val);
+        }
+    }
+    for(int i = 0; i < n; i++) if(vx[i]) lx[i] -= mn;
+    for(int i = 0; i < n; i++) if(vy[i]) ly[i] += mn;
+}
+
+void hungarian(){
+    for(int i = 0; i < n; i++){
+        while(true){
+            fill(vx, vx + n, 0);
+            fill(vy, vy + n, 0);
+            if(dfs(i)) break;
+            relabel();
+        }
+    }
+    ll ans = 0;
+    for(int i = 0; i < n; i++){
+        ans += w[match[i]][i];
+    }
+    cout<<ans<<endl;
+    for(int i = 0; i < n; i++) cout<<lx[i]<<" ";
+    cout<<endl;
+    for(int i = 0; i < n; i++) cout<<ly[i]<<" ";
+}
+
 int main(void){
-	cin>>n;
-	
-	mt.assign(k, -1);
-	vector<bool> used1(n, false);
-	for(int i = 0; i <n; i++){
-		for(auto x : adj[i]){
-			if(mt[x] == -1){
-				mt[x] = i;
-				used[i] = true;
-				break;
-			}
-		}
-	}
-	for(int i = 0; i < n; i++){
-		if(used1[i]) continue;
-		used.assign(n, false);
-		try_kuhn(i);
-	}
-	
-	for(int i = 0; i < k; i++){
-		if(mt[i] != -1){
-			cout<<mt[i] + 1<<" "<<i + 1<<endl;
-		}
-	}
+    cin>>n;
+    w.resize(n, vector<ll>(n));
+    fill(match, match + n, -1);
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin>>w[i][j];
+            lx[i] = max(lx[i], w[i][j]);
+        }
+    }
+    hungarian();
 }
