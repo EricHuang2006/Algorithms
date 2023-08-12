41ee7c05dd04b4468614c57083980fe09fe01fe3
Phoenix0423 <34877707+EricHuang2006@users.noreply.github.com>
2023-08-12 10:53:40 +0800

    Update Basics.cpp

--- Geometry/Basics.cpp
+++ Geometry/Basics.cpp
@@ -1,32 +1,59 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-struct Point{
-	
+struct pt{
 	double x, y;
-	Point (double x = 0, double y = 0) : x(x), y(y){}
+	pt (double x = 0, double y = 0) : x(x), y(y){}
 };
+typedef pt vec;
 
-typedef Point Vector;
-
-Vector operator + (Vector A, Vector B){
-	return Vector(A.x + B.x, A.y + B.y);
+vec operator + (vec A, vec B){
+	return vec(A.x + B.x, A.y + B.y);
 }
 
-Vector operator - (Vector A, Vector B){
-	return Vector(A.x - B.x, A.y - B.y);
+vec operator - (vec A, vec B){
+	return vec(A.x - B.x, A.y - B.y);
 }
 
-Vector operator * (Vector A, double p){
-	return Vector(A.x * p, A.y * p);
+vec operator * (vec A, double p){
+	return vec(A.x * p, A.y * p);
 }
 
-Vector operator / (Vector A, double p){
-	return Vector(A.x / p, A.y / p);
+vec operator / (vec A, double p){
+	return vec(A.x / p, A.y / p);
 }
 
-bool operator < (const Point &a, const Point &b) {
+bool operator < (const pt &a, const pt &b) {
 	return a.x < b.x || (a.x == b.x && a.y < b.y);
+}
+
+double dot(vec A, vec B){
+	return A.x * B.x + A.y * B.y;	
+}
+
+double len(vec A){
+	return sqrt(dot(A, A));	
+}	
+
+double angle(vec A, vec B){
+	return acos(dot(A, B) / len(A) / len(B));
+}
+
+double cross(vec A, vec B){
+	return A.x * B.y - A.y * B.x;
+}
+
+double Area2(pt A, pt B, pt C){
+	return cross(B - A, C - A);
+}
+
+vec Rotate(vec A, double rad){
+	return vec(A.x * cos(rad) - A.y * sin(rad), A.x * sin(rad) + A.y * cos(rad));
+}
+
+vec Normal(vec A){ // unit normal of vector
+	double L = len(A);
+	return vec(-A.y / L, A.x / L);
 }
 
 const double eps = 1e-10;
@@ -35,42 +62,42 @@ int dcmp(double x){
 	else return x < 0? -1 : 1;
 }
 
-bool operator == (const Point &a, const Point &b) {
+bool operator == (const pt &a, const pt &b) {
 	return dcmp(a.x - b.x) == 0 && dcmp(a.x - b.y) == 0;
 }
 
-double dot(Vector A, Vector B){
-	return A.x * B.x + A.y * B.y;	
+inline int ori(pt a, pt b, pt c){
+	double area = cross(b - a, c - a);
+	if(area > -eps && area < eps) return 0;
+	return area > 0 ? 1 : -1;
 }
 
-double length(Vector A){
-	return sqrt(dot(A, A));	
-}	
-
-double angle(Vector A, Vector B){
-	return acos(dot(A, B) / length(A) / length(B));
+inline int btw(pt a, pt b, pt c){ // return if c is between a and b
+	if(fabs(cross(b - a, c - a)) > eps) return false;
+	if(dot(b - a, c - a) > -eps && len(c - a) <= len(b - a)) return true;
+	return false;
 }
 
-double cross(Vector A, Vector B){
-	return A.x * B.y - A.y * B.x;
+bool intersect(pt a, pt b, pt c, pt d){
+	if(a == c || a == d || b == c || b == d) return true;
+	int a123 = ori(a, b, c), a124 = ori(a, b, d), a341 = ori(c, d, a), a342 = ori(c, d, b);
+	if(a123 == 0 && a124 == 0){
+		if(btw(a, b, c) || btw(a, b, d) || btw(c, d, a) || btw(c, d, b)) return true;
+		else return false;
+	}
+	else if(a123 * a124 <= 0 && a341 * a342 <= 0) return true;
+	return false;
 }
 
-double Area2(Point A, Point B, Point C){
-	return cross(B - A, C - A);
-}
-
-Vector Rotate(Vector A, double rad){
-	return Vector(A.x * cos(rad) - A.y * sin(rad), A.x * sin(rad) + A.y * cos(rad));
-}
-Vector Normal(Vector A){ // unit normal of vector
-	double L = length(A);
-	return Vector(-A.y / L, A.x / L);
+istream &operator>>(istream &s, pt &a){
+	s >> a.x >> a.y;
+	return s;
 }
 
 int main(void){
 	
-	Vector A;
-	while(cin>>A.x>>A.y){
-		cout<<length(A)<<endl;
+	vec A;
+	while(cin >> A){
+		cout<<len(A)<<endl;
 	}
 }
