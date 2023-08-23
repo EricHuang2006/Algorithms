b8a71019eea5cb050229c19d1e79ce54638a8220
Phoenix0423 <34877707+EricHuang2006@users.noreply.github.com>
2023-08-23 18:38:43 +0800

    Create XOR Basis.cpp

---
+++ Mathematics/XOR Basis.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio ios::sync_with_stdio(false), cin.tie(0)
+#define pb push_back
+int basis[30]; // basis[i] : 最高bit為i的一組basis
+
+bool add(int x){
+	for(int i = 29; i >= 0; i--){
+		if(x & (1 << i)){
+			if(!basis[i]){
+				basis[i] = x;
+				return true;
+			}
+			x ^= basis[i];
+		}
+	}
+	return false;
+}
+
+int find(int x){
+	for(int i = 29; i >= 0; i--){
+		if(x & (1 << i)) x ^= basis[i];
+	}
+	return x == 0;
+}
+
+int mx(){
+  int mx = 0;
+  for(int i = 29; i >= 0; i--){
+    if((x & (1 << i) == 0 && basis[i]) mx ^= basis[i];
+  }
+  return mx;
+}
