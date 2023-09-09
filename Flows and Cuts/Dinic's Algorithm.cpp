6b92af49bfebca9e2e991456272ae26b55c74c29
Phoenix0423 <34877707+EricHuang2006@users.noreply.github.com>
2023-09-09 18:39:20 +0800

    Update Dinic's Algorithm.cpp

--- Flows and Cuts/Dinic's Algorithm.cpp
+++ Flows and Cuts/Dinic's Algorithm.cpp
@@ -1,74 +1,94 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+const ll INF = 1e18;
+
 struct Dinic{
-
-    static const int N = 1e5;
-    static const int INF = 1e9 + 7;
-    struct edge{
-        int to, cap, rev;
-        edge(){}
-        edge(int _to, ll _cap, int _rev) : to(_to), cap(_cap), rev(_rev){}
-    };
-    vector<edge> g[N];
-    void add_edge(int from, int to, int cap){
-        g[from].push_back(edge(to, cap, int(g[to].size())));
-        g[to].push_back(edge(from, 0, int(g[from].size() - 1)));
-    }
-    int n, s, t;
-    void init(int _n, int _s, int _t) {
-        n = _n, s = _s, t = _t;
-        for(int i = 0; i <= n; i++){
-            g[i].clear();
-        }
-    }
-    int level[N], iter[N]; //level[i] = d[i], iter[i] = m[i]
-
-    void bfs(){
-        memset(level, -1, sizeof(level));
-        level[s] = 0;
-        queue<int> q;
-        q.push(s);
-        while(!q.empty()){
-            int cur = q.front(); q.pop();
-            for(auto e : g[cur]){
-                if(e.cap > 0 && level[e.to] == -1){
-                    level[e.to] = level[cur] + 1;
-                    q.push(e.to);
-                }
-            }
-        }
-    }
-    ll dfs(int now, int flow){
-        if(now == t) return flow; // reached termination, return flow
-        for(int& i = iter[now]; i < g[now].size(); i++){
-            edge &e = g[now][i];
-            if(e.cap > 0 && level[e.to] == level[now] + 1){
-            //    cout<<now<<" "<<flow<<" "<<e.cap<<endl;
-                int ret = dfs(e.to, min(flow, e.cap));
-                if(ret > 0){ // successfully found an augmenting path
-                    e.cap -= ret;
-                    g[e.to][e.rev].cap += ret;
-                    return ret;
-                }
-            }
-        }
-        return 0;
-    }
-    ll flow(){
-        ll ret = 0;
-        while(true){
-            bfs();
-            if(level[t] == -1) break;
-            memset(iter, 0, sizeof(iter));
-            ll tmp = 0;
-            while((tmp = dfs(s, INF)) > 0){
-                ret += tmp;
-            }
-        }
-        return ret;
-    }
+	struct edge{
+		ll to, cap;
+		edge(){}
+		edge(int _to, ll _cap) : to(_to), cap(_cap){}
+	};
+	vector<edge> e;
+	vector<vector<int>> adj;
+	vector<int> iter, level;
+	int n, s, t;
+	void init(int _n, int _s, int _t){
+		n = _n, s = _s, t = _t;
+		adj = vector<vector<int>>(n);
+		iter = vector<int>(n);
+		level = vector<int>(n);
+		e.clear();
+	}
+	void add_edge(int from, int to, ll cap){
+		adj[from].pb(e.size()), adj[to].pb(e.size() + 1);
+		e.pb(edge(to, cap)), e.pb(edge(from, 0));
+	}
+	void bfs(){
+		fill(level.begin(), level.end(), -1);
+		level[s] = 0;
+		queue<int> q;
+		q.push(s);
+		while(!q.empty()){
+			int cur = q.front(); q.pop();
+			for(auto id : adj[cur]){
+				auto [to, cap] = e[id];
+				if(level[to] == -1 && cap){
+					level[to] = level[cur] + 1;
+					q.push(to);
+				}
+			}
+		}
+	}
+	ll dfs(int pos, ll flow){
+		if(pos == t) return flow;
+		for(int &i = iter[pos]; i < adj[pos].size(); i++){
+			auto [to, cap] = e[adj[pos][i]];
+			if(level[to] == level[pos] + 1 && cap){
+				ll tmp = dfs(to, min(flow, cap));
+				if(tmp){
+					e[adj[pos][i]].cap -= tmp;
+					e[adj[pos][i] ^ 1].cap += tmp;
+					return tmp; 
+				}
+			}
+		}
+		return 0;
+	}
+	ll flow(){
+		ll ret = 0;
+		while(true){
+			bfs();
+			if(level[t] == -1) break;
+			fill(iter.begin(), iter.end(), 0);
+			ll tmp;
+			while((tmp = dfs(s, INF)) > 0){
+				ret += tmp;
+			}
+		}
+		return ret;
+	}
+	vector<pll> cut(){
+		vector<pll> ret;
+		fill(level.begin(), level.end(), -1);
+		level[s] = 0;
+		queue<int> q;
+		q.push(s);
+		while(!q.empty()){
+			int cur = q.front(); q.pop();
+			for(auto id : adj[cur]){
+				auto [to, cap] = e[id];
+				if(cap == 0 && level[to] == -1) ret.pb({e[id ^ 1].to, to});
+				else if(level[to] == -1){
+					level[to] = level[cur] + 1;
+					q.push(to);
+				}
+			}
+		}
+		return ret;
+	}
 } flow;
+
 
 int main(){
     int n, m;
