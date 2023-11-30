bea67e56c3b522cab83949e5b8cae96d46682cb9
Phoenix0423 <34877707+EricHuang2006@users.noreply.github.com>
2023-11-30 12:41:00 +0800

    Update Centroid Decomposition.cpp

--- Data Structure/Centroid Decomposition.cpp
+++ Data Structure/Centroid Decomposition.cpp
@@ -1,108 +1,95 @@
-//經典題：CF 342E - Xenia and Tree https://codeforces.com/problemset/problem/342/E (also can use sqrt decomposition)
+/*
+	* Date : 2023.11.30
+	* 點分樹
+ 	* 題目 : TIOJ 1171 . 我要成為海賊王
+*/
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 typedef pair<int, int> pll;
+#define fastio ios::sync_with_stdio(false), cin.tie(0)
+#pragma GCC optimize("Ofast")
 #define pb push_back
 #define eb emplace_back
-const int maxn = 2e5 + 5;
-int n, m;
-vector<int> adj[maxn];
-int sz[maxn], dep[maxn], cd_par[maxn], del[maxn], best[maxn], succ[maxn][19];
+#define f first
+#define s second
+#define lowbit(x) x&-x
+const int INF = 1e9;
+const int maxn = 1e5 + 5, k = 20;
+int n, q;
+vector<pll> adj[maxn];
+ll dist[k][maxn]; // dist[k][i] = distance of i to k-th-layer-par in the centroid tree(k is bounded by logn)
+int sz[maxn], del[maxn], par[maxn], cdep[maxn];
+ll sum[maxn], cnt[maxn], re[maxn]; // re : 存此子樹中的結點對父親的貢獻
 
 void dfssz(int pos, int prev){
-	sz[pos] = 1;
-	for(auto x : adj[pos]){
-		if(x == prev || del[x]) continue;
-		dfssz(x, pos);
-		sz[pos] += sz[x];
-	}
+    sz[pos] = 1;
+    for(auto [x, w] : adj[pos]){
+        if(del[x] || x == prev) continue;
+        dfssz(x, pos);
+        sz[pos] += sz[x];
+    }
 }
-int get_centroid(int pos, int prev, int cnt){
-	//cnt = the entire subtree size
-	for(auto x : adj[pos]){
-		if(x != prev && !del[x] && sz[x] > cnt / 2) return get_centroid(x, pos, cnt);
-	}
-	return pos;
+int get_centroid(int pos, int prev, int siz){
+    for(auto [x, w] : adj[pos]){
+        if(!del[x] && x != prev && sz[x] > siz / 2) return get_centroid(x, pos, siz);
+    }
+    return pos;
 }
-void centroid_decomposition(int pos, int anc){
-	dfssz(pos, anc);
-	int centroid = get_centroid(pos, anc, sz[pos]);
-	cd_par[centroid] = anc, del[centroid] = 1;
-	for(auto x : adj[centroid]){
-		if(!del[x]) centroid_decomposition(x, centroid);
-	}
+void get_dist(int pos, int prev, int layer){
+    for(auto [x, w] : adj[pos]){
+        if(del[x] || x == prev) continue;
+        dist[layer][x] = dist[layer][pos] + w;
+        get_dist(x, pos, layer);
+    }
 }
-void dfsdep(int pos, int prev){
-	succ[pos][0] = prev;
-	for(int i = 1; i < 19; i++) succ[pos][i] = succ[succ[pos][i - 1]][i - 1];
-	for(auto x : adj[pos]){
-		if(x == prev) continue;
-		dep[x] = dep[pos] + 1;
-		dfsdep(x, pos);
-	}
+void cd(int pos, int layer = 1, int p = 0){
+    dfssz(pos, -1);
+    int cen = get_centroid(pos, -1, sz[pos]);
+    del[cen] = 1;
+    dist[layer][cen] = 0;
+    cdep[cen] = layer;
+    par[cen] = p;
+    get_dist(cen, -1, layer);
+    for(auto [x, w] : adj[cen]){
+        if(!del[x]){
+            cd(x, layer + 1, cen);
+        }
+    }
 }
-int main(){
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cin>>n>>m;
-	for(int i = 0; i < n - 1; i++){
-		int a, b;
-		cin>>a>>b;
-		a--, b--;
-		adj[a].pb(b);
-		adj[b].pb(a);
-	}
-	centroid_decomposition(0, -1);
-	dfsdep(0, 0); // also compute the ancestor relationship of the original graph
-	for(int i = 0; i < n; i++) best[i] = 1e9;
-	function<int(int, int)> find_lca = [&](int a, int b){
-		for(int i = 18; i >= 0; i--){
-			if(succ[a][i] != succ[b][i]){
-				a = succ[a][i];
-				b = succ[b][i];
-			}
-		}
-		return succ[a][0];
-	};
-	function<int(int, int)> lift = [&](int b, int steps){
-		for(int i = 0; i < 19; i++) if(steps & (1 << i)) b = succ[b][i];
-		return b;
-	};
-	function<int(int, int)> lca = [&](int a, int b){
-		if(dep[a] > dep[b]) swap(a, b);
-		b = lift(b, dep[b] - dep[a]);
-		if(a == b) return a;
-		else return find_lca(a, b);
-	};
-	function<int(int, int)> dist = [&](int a, int b){
-		int c = lca(a, b);
-		return dep[a] + dep[b] - dep[c] * 2;
-	};
-	function<void(int)> upd = [&](int pos){
-		int cur = pos;
-		while(cur != -1){
-			best[cur] = min(best[cur], dist(cur, pos));
-			cur = cd_par[cur];
-		}
-	};
-	upd(0);
-	while(m--){
-		int t, a;
-		cin>>t>>a;
-		a--;
-		if(t == 1){
-			upd(a);
-		}
-		else{
-			int cur = a, ans = 1e9;
-			while(cur != -1){
-				ans = min(ans, best[cur] + dist(a, cur));
-				cur = cd_par[cur];
-			}
-			cout<<ans<<"\n";
-		}
-	}
-	
+void upd(int p){
+    for(int x = p, d = cdep[x]; d; x = par[x], d--){
+        sum[x] += dist[d][p];
+        re[x] += dist[d - 1][p];
+        cnt[x] ++;
+    }
+}
+ll qry(int p){
+    ll pre = 0, ans = 0;
+    for(int x = p, d = cdep[x]; d; x = par[x], d--){
+        ans += sum[x] - re[x] + (cnt[x] - pre) * dist[d][p];
+        pre = cnt[x];
+    }
+    return ans;
+}
 
-}		
+bitset<maxn> color;
+int main(void){
+    fastio;
+    cin>>n>>q;
+    for(int i = 0; i < n - 1; i++){
+        int a, b, l;
+        cin>>a>>b>>l;
+        adj[a].eb(b, l);
+        adj[b].eb(a, l);
+    }
+    cd(0);
+    for(int i = 0; i < q; i++){
+        int t, x;
+        cin>>t>>x;
+        if(t == 1){
+            if(!color[x]) color[x] = true, upd(x);
+        }
+        else cout<<qry(x)<<"\n";
+    }
+}
