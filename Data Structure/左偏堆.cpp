7f23e2e731c175228169acba9946e8fd07e110d2
Phoenix0423 <34877707+EricHuang2006@users.noreply.github.com>
2024-05-07 12:45:42 +0800

    Update 左偏堆.cpp

--- Data Structure/左偏堆.cpp
+++ Data Structure/左偏堆.cpp
@@ -5,16 +5,23 @@
   Can be made persistent
 */
 
-struct node{
-	int ch[2], val, d;
-	node(){}
-} st[maxn];
-
-int merge(int x, int y){
-	if(!x || !y) return x + y;
-	if(st[x].val > st[y].val) swap(x, y);
-	st[x].ch[1] = merge(st[x].ch[1], y);
-	if(st[st[x].ch[0]].d < st[st[x].ch[1]].d) swap(st[x].ch[0], st[x].ch[1]);
-	st[x].d = st[st[x].ch[1]].d + 1;
-	return x;
-}
+struct LeftistTree{
+	int cnt, rt[maxn], lc[maxn * 20], rc[maxn * 20], d[maxn * 20];
+	int v[maxn * 20];
+	LeftistTree(){}
+	int newnode(pll nd){
+		cnt++;
+		v[cnt] = nd;
+		return cnt;
+	}
+	int merge(int x, int y){
+		if(!x || !y) return x + y;
+		if(v[x] > v[y]) swap(x, y);
+		int p = ++cnt;
+		lc[p] = lc[x], v[p] = v[x];
+		rc[p] = merge(rc[x], y);
+		if(d[lc[p]] < d[rc[p]]) swap(lc[p], rc[p]);
+		d[p] = d[rc[p]] + 1;
+		return p;
+	}
+} st;
