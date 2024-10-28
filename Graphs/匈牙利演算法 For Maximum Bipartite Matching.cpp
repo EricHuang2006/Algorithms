78db65fc9ee5a0c98c3d01cea287afbc6f263c7f
Phoenix0423 <34877707+EricHuang2006@users.noreply.github.com>
2024-10-28 21:19:10 +0800

    Update 匈牙利演算法 For Maximum Bipartite Matching.cpp

--- Graphs/匈牙利演算法 For Maximum Bipartite Matching.cpp
+++ Graphs/匈牙利演算法 For Maximum Bipartite Matching.cpp
@@ -1,72 +1,92 @@
-//TIOJ 2197. E. 密室逃脫 https://tioj.ck.tp.edu.tw/problems/2197
-#include<bits/stdc++.h>
-using namespace std;
-const int maxn = 600;
-typedef long long ll;
-typedef pair<ll, ll> pll;
-ll lx[600], ly[600], vx[600], vy[600], match[600];
-int n;
-vector<vector<ll>> w;
+/*   
+    Updated : 2024.10.28
+    Description : Hungarian algorithm for maximum perfect bipartite matching in O(n^3)
+    Verification : Library Checker - Assignment Problem https://judge.yosupo.jp/problem/assignment
+*/
 
-int dfs(int x){
-    //cout<<"dfs : "<<x<<endl;
-    if(vx[x]) return false;
-    vx[x] = 1;
-    for(int i = 0; i < n; i++){
-        if(lx[x] + ly[i] == w[x][i] && !vy[i]){
-            vy[i] = true;
-            if(match[i] == -1 || dfs(match[i])){
-                match[i] = x;
-                return true;
+struct Hungarian{
+    using T = ll;
+    vector<T> lx, ly, slack;
+    vector<int> vx, vy, match;
+    vector<vector<T>> w;
+    queue<int> q;
+    int n;
+    void init(int _n){
+        n = _n;
+        lx.resize(n), ly.resize(n), slack.resize(n);
+        vx.resize(n), vy.resize(n), match.resize(n, -1);
+        w.resize(n, vector<T>(n));
+    }
+    void inp(int x, int y, int val){
+        w[x][y] = val;
+        lx[x] = max(lx[x], val);
+    }
+    int dfs(int x){
+        if(vx[x]) return false;
+        vx[x] = 1;
+        for(int i = 0; i < n; i++){
+            if(lx[x] + ly[i] == w[x][i] && !vy[i]){
+                vy[i] = true;
+                if(match[i] == -1 || dfs(match[i])){
+                    match[i] = x;
+                    return true;
+                }
+            }
+
+        }
+        return false;
+    }
+    int pdfs(int x){
+        fill(vx.begin(), vx.end(), 0);
+        fill(vy.begin(), vy.end(), 0);
+        return dfs(x);
+    }
+    void upd(int x){
+        for(int i = 0; i < n; i++){
+            if(!slack[i]) continue;
+            slack[i] = min(slack[i], lx[x] + ly[i] - w[x][i]);
+            if(!slack[i] && !vy[i]) q.push(i);
+        }
+    }
+    void relabel(){
+        T mn = numeric_limits<T>::max() / 3;
+        for(int i = 0; i < n; i++){
+            if(!vy[i]) mn = min(mn, slack[i]);
+        }
+        for(int i = 0; i < n; i++){
+            if(vx[i]) lx[i] -= mn;
+            if(vy[i]) ly[i] += mn;
+            else{
+                slack[i] -= mn;
+                if(!slack[i]) q.push(i);
             }
         }
-
     }
-    return false;
-}
-
-void relabel(){
-    int mn = 1e9;
-    for(int i = 0; i < n; i++){
-        if(!vx[i]) continue;
-        for(int j = 0; j < n; j++){
-            if(vy[j]) continue;
-            int val = lx[i] + ly[j] - w[i][j];
-            mn = min(mn, val);
+    auto solve(){
+        for(int i = 0; i < n; i++){
+            if(pdfs(i)) continue;
+            while(!q.empty()) q.pop();
+            fill(slack.begin(), slack.end(), INF);
+            for(int j = 0; j < n; j++) if(vx[j]) upd(j);
+            int ok = 0;
+            while(!ok){
+                relabel();
+                while(!q.empty()){
+                    int j = q.front(); q.pop();
+                    if(match[j] == -1){
+                        pdfs(i);
+                        ok = 1;
+                        break;
+                    }
+                    vy[j] = vx[match[j]] = 1, upd(match[j]);
+                }
+            }
         }
-    }
-    for(int i = 0; i < n; i++) if(vx[i]) lx[i] -= mn;
-    for(int i = 0; i < n; i++) if(vy[i]) ly[i] += mn;
-}
-
-void hungarian(){
-    for(int i = 0; i < n; i++){
-        while(true){
-            fill(vx, vx + n, 0);
-            fill(vy, vy + n, 0);
-            if(dfs(i)) break;
-            relabel();
+        T ans = 0;
+        for(int i = 0; i < n; i++){
+            ans += w[match[i]][i];
         }
+        for(int i = 0; i < n; i++) lx[match[i]] = i;
+        return make_pair(ans, lx);
     }
-    ll ans = 0;
-    for(int i = 0; i < n; i++){
-        ans += w[match[i]][i];
-    }
-    cout<<ans<<endl;
-    for(int i = 0; i < n; i++) cout<<lx[i]<<" ";
-    cout<<endl;
-    for(int i = 0; i < n; i++) cout<<ly[i]<<" ";
-}
-
-int main(void){
-    cin>>n;
-    w.resize(n, vector<ll>(n));
-    fill(match, match + n, -1);
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            cin>>w[i][j];
-            lx[i] = max(lx[i], w[i][j]);
-        }
-    }
-    hungarian();
-}
+} h;
